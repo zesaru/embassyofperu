0c87da07cec42c2f3e81207f4df4fe34befde0ce
Cesar Murillo <namiayumis@gmail.com>
2020-06-08 21:47:26 +0900

    install cmd-dev contenful

---
+++ src/pages/actividades.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const actividades = () => {
+  return (
+    <Layout>
+      <h1>Actividades del Embajador</h1>
+    </Layout>
+  )
+}
+
+export default actividades
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,50 +1,46 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery } from "gatsby"
 import Layout from "../components/layout"
 import Imagen from "../components/image"
 
 const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulCategories(sort: { fields: name }) {
+        edges {
+          node {
+            id
+            name
+            slug
+            imageCategory {
+              file {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
   return (
     <Layout>
       <Imagen></Imagen>
       <section className="secundary-menu-container">
         <ul>
-          <li>
-            <Link to="#">
-              <img height="55px" src="./img/meeting.png" alt="" />
-              <span> Actividades del Embajador</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="#">
-              <img height="55px" src="./img/gastro.png" alt="" />
-              <span>Gastronomía</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="#">
-              <img height="55px" src="./img/turismo.png" alt="" />
-              <span>Turismo</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="#">
-              <img height="55px" src="./img/comercio.png" alt="" />
-              <span>Comercio</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="#">
-              <img height="55px" src="./img/cultura.png" alt="" />
-              <span>Cultura</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="#">
-              <img height="55x" src="./img/consulados.png" alt="" />
-              <span>Consulados</span>
-            </Link>
-          </li>
+          {data.allContentfulCategories.edges.map(edge => {
+            return (
+              <li>
+                <Link to={edge.node.slug}>
+                  <img
+                    height="55px"
+                    src={edge.node.imageCategory.file.url}
+                    alt={edge.node.name}
+                  />
+                  <span> {edge.node.name}</span>
+                </Link>
+              </li>
+            )
+          })}
         </ul>
       </section>
       <div className="social">
