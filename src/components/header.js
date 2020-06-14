2f980e74425c4adf0e728b7c46e46b8a9dd95490
Cesar Murillo <namiayumis@gmail.com>
2020-06-14 18:49:56 +0900

    start laptop response

--- src/components/header.js
+++ src/components/header.js
@@ -4,33 +4,35 @@ import Escudo from "./escudo"
 
 const Header = () => (
   <header>
-    <nav>
-      <div className="logo">
-        <Escudo></Escudo>
-        <div className="red-bar"></div>
-        <h1>
-          EMBAJADA DEL PERÚ <span>EN JAPÓN</span>
-        </h1>
-      </div>
-      <div className="nav-language"></div>
-      <div className="nav-social"></div>
-      <input type="checkbox" id="menu" className="menu__checkbox" />
-      <ul className="menu">
-        <li className="menu__item">
-          <Link to="/">INICIO</Link>
-        </li>
-        <li className="menu__item">
-          <Link to="#">EMBAJADOR</Link>
-        </li>
-        <li className="menu__item">
-          <Link to="#">CONSULADOS</Link>
-        </li>
-        <li className="menu__item">
-          <Link to="#">RESOLUCIONES</Link>
-        </li>
-      </ul>
-      <label htmlFor="menu" className="menu__button"></label>
-    </nav>
+    <div className="wrapper">
+      <nav>
+        <div className="logo">
+          <Escudo></Escudo>
+          <div className="red-bar"></div>
+          <h1>
+            EMBAJADA DEL PERÚ <span>EN JAPÓN</span>
+          </h1>
+        </div>
+        <div className="nav-language"></div>
+        <div className="nav-social"></div>
+        <input type="checkbox" id="menu" className="menu__checkbox" />
+        <ul className="menu">
+          <li className="menu__item">
+            <Link to="/">INICIO</Link>
+          </li>
+          <li className="menu__item">
+            <Link to="#">EMBAJADOR</Link>
+          </li>
+          <li className="menu__item">
+            <Link to="#">CONSULADOS</Link>
+          </li>
+          <li className="menu__item">
+            <Link to="#">RESOLUCIONES</Link>
+          </li>
+        </ul>
+        <label htmlFor="menu" className="menu__button"></label>
+      </nav>
+    </div>
   </header>
 )
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from "../components/layout"
 import Imagen from "../components/image"
 import Social from "../components/social"
 import Img from "gatsby-image"
+import "./index.scss"
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
@@ -34,32 +35,37 @@ const IndexPage = () => {
   `)
   return (
     <Layout>
-      <Imagen></Imagen>
-      <section className="secundary-menu-container">
-        <ul>
-          {data.allContentfulCategories.edges.map(edge => {
-            return (
-              <li key={edge.node.id}>
-                <Link to={edge.node.slug}>
-                  <img
-                    className="icon"
-                    height="55px"
-                    src={edge.node.imageCategory.file.url}
-                    alt={edge.node.name}
-                  />
-                  <span> {edge.node.name}</span>
-                  {edge.node.picture === null ? (
-                    console.log("nulo")
-                  ) : (
-                    <Img className="picture" fluid={edge.node.picture.fluid} />
-                  )}
-                </Link>
-              </li>
-            )
-          })}
-        </ul>
-      </section>
-      <Social></Social>
+      <div className="wrapper">
+        <Imagen></Imagen>
+        <section className="secundary-menu-container">
+          <ul>
+            {data.allContentfulCategories.edges.map(edge => {
+              return (
+                <li key={edge.node.id}>
+                  <Link to={edge.node.slug}>
+                    <img
+                      className="icon"
+                      height="55px"
+                      src={edge.node.imageCategory.file.url}
+                      alt={edge.node.name}
+                    />
+                    <span> {edge.node.name}</span>
+                    {edge.node.picture === null ? (
+                      console.log("nulo")
+                    ) : (
+                      <Img
+                        className="picture"
+                        fluid={edge.node.picture.fluid}
+                      />
+                    )}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </section>
+        <Social></Social>
+      </div>
     </Layout>
   )
 }
