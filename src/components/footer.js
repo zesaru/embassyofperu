30df473408ede9f57c5ba92e7561d96aca70d986
zesaru <namiayumis@gmail.com>
2020-06-17 00:40:57 -0700

    footer 3 colums responsive

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,19 +1,32 @@
 import React from "react"
+import "./footer.scss"
 
 export default function footer() {
   return (
     <footer>
       <div className="enlacesdeinteres">
-        <span>ENLACES DE INTERES</span>
-        <p>Ministerio de Relaciones Exteriores</p>
-        <p>Gobierno del Perú</p>
-        <p>Ministerio de Comercio y Turismo</p>
+        <h4>ENLACES DE INTERES</h4>
+        <div className="footer-enlaces">
+          <h5>Ministerio de Relaciones Exteriores</h5>
+          <h5>Gobierno del Perú</h5>
+          <h5>Ministerio de Comercio y Turismo</h5>
+        </div>
       </div>
       <div className="contacto">
-        <span>CONTACTO</span>
-        <p>Dirección: 150-0012 Tokio Shibuya-ku Hiroo 2-3-1</p>
-        <p>Teléfono: 03-3406-4243</p>
-        <p>Fax: 03-3409-7589</p>
+        <h4>CONTACTO</h4>
+        <div className="footer-enlaces">
+          <h5>Dirección: 150-0012 Tokio Shibuya-ku Hiroo 2-3-1</h5>
+          <h5>Teléfono: 03-3406-4243</h5>
+          <h5>Fax: 03-3409-7589</h5>
+        </div>
+      </div>
+      <div className="intituciones">
+        <h4>Intituciondes del Perú en Japon</h4>
+        <div className="footer-enlaces">
+          <h5>Consulado General del Peru en Tokio</h5>
+          <h5>Consulado General del Peru en Nagoya</h5>
+          <h5>Seccion Comercial de la Embajada </h5>
+        </div>
       </div>
     </footer>
   )
