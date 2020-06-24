48cdc1334ba488ad0b667bdd2730c896fcb48c0f
Cesar Murillo <namiayumis@gmail.com>
2020-06-24 12:24:12 +0900

    funcionarios

--- src/pages/funcionarios.js
+++ src/pages/funcionarios.js
@@ -4,7 +4,57 @@ import Layout from "../components/layout"
 const funcionarios = () => {
   return (
     <Layout>
-      <h2>Funcionarios</h2>
+      <div className="wrapper">
+        <h2>Funcionarios</h2>
+        <p>
+          Ministro Consejero , Mario Bustamante <br />
+          <strong>Jefe de Cancillería</strong>
+        </p>
+        <p>
+          Consejera, Marita Puertas <br />
+          <strong>
+            Encargada de asuntos económicos-comerciales, Organismos, medio
+            ambiente y cooperación.
+          </strong>
+        </p>
+        <p>
+          Capitán de Navío, Eduardo Silva Marzuka <br />
+          <strong>Agregado de Defensa y Naval</strong>
+        </p>
+        <p>
+          Fernando Albareda <br />
+          <strong>Consejero Comercial</strong>
+        </p>
+        <p>
+          Primer Secretario, Enrique Descalzi <br />
+          <strong>
+            Encargado de asuntos Políticos, Cooperación Judicial, Científica y
+            Tecnológica.
+          </strong>
+        </p>
+        <p>
+          Tercer Secretario, Amador Pantoja <br />
+          <strong>
+            Encargado de la Sección Cultural de la Embajada y gestión de riesgos
+            de desastres .
+          </strong>
+        </p>
+        <p>
+          Tercer Secretario, Amador Pantoja <br />
+          <strong>
+            Encargado de la Sección Cultural de la Embajada y gestión de riesgos
+            de desastres .
+          </strong>
+        </p>
+        <p>
+          Tercer Secretario, Alejandro Hidalgo <br />
+          <strong></strong>
+        </p>
+        <p>
+          William Valderrama <br />
+          <strong>Especialista Agrícola de la Sección Comercial</strong>
+        </p>
+      </div>
     </Layout>
   )
 }
