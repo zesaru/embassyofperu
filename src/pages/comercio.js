0fe931ffa719a0ff4546c71b67ad150ca32c8dad
Cesar Murillo <namiayumis@gmail.com>
2020-07-02 11:07:49 +0900

    page comercio query

--- src/pages/comercio.js
+++ src/pages/comercio.js
@@ -1,22 +1,61 @@
 import React from "react"
-import { graphql, useStaticQuery } from "gatsby"
+import { graphql, useStaticQuery, Link } from "gatsby"
 import Layout from "../components/layout"
 import Banner from "../components/banner"
 import Image from "gatsby-image"
 import "./actividades.scss"
 
-const Comercio = () => {
+const Comunicados = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulPosts(
+        filter: { categoryId: { slug: { eq: "comercio" } } }
+        sort: { fields: datePost, order: DESC }
+      ) {
+        nodes {
+          categoryId {
+            name
+            slug
+          }
+          id
+          name
+          slug
+          images {
+            file {
+              url
+            }
+            fluid(maxWidth: 800) {
+              ...GatsbyContentfulFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+  `)
   return (
     <Layout>
       <div className="wrapper">
         <Banner></Banner>
         <h2>Comercio</h2>
-        <div className="content_item">
-
+        <div className="content">
+          {data.allContentfulPosts.nodes.map(node => {
+            return (
+              <div className="content_item" key={node.id}>
+                <Link to={`/${node.slug}`}>
+                  <div className="image_categories" key={node.id}>
+                    <div className="content_title" key={node.id}>
+                      <p>{node.name}</p>
+                    </div>
+                    <Image fluid={node.images[0].fluid} alt="" />
+                  </div>
+                </Link>
+              </div>
+            )
+          })}
         </div>
       </div>
     </Layout>
-    );
+  )
 }
 
-export default Comercio;
+export default Comunicados
