9a650ece2851087e6c811838b604a0725a51e802
Cesar Murillo <namiayumis@gmail.com>
2020-07-13 21:50:51 +0900

    install and create sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,10 @@ require("dotenv").config({
 module.exports = {
   siteMetadata: {
     title: `Embassy of Peru`,
-    description: `This is the oficial Web of the Embassy.`,
+    description: `Pagina oficial de la Embajada del Perú en Japón.`,
     author: `@zesarum`,
-    url: `www.embajadadelperuenjapon.org`,
+    url: `http://www.embajadadelperuenjapon.org`,
+    siteUrl: `http://www.embajadadelperuenjapon.org`,
   },
   plugins: [
     {
@@ -35,6 +36,40 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        // Exclude specific pages or groups of pages using glob parameters
+        // See: https://github.com/isaacs/minimatch
+        // The example below will exclude the single `path/to/page` and all routes beginning with `category`
+        query: `
+          {
+            site {
+              siteMetadata {
+                url
+              }
+            }
+  
+            allSitePage {
+              nodes {
+                path
+              }
+            }
+        }`,
+        resolveSiteUrl: ({ site }) => {
+          //Alternativly, you may also pass in an environment variable (or any location) at the beginning of your `gatsby-config.js`.
+          return site.siteMetadata.url
+        },
+        serialize: ({ site, allSitePage }) =>
+          allSitePage.nodes.map(node => {
+            return {
+              url: `${site.siteMetadata.siteUrl}${node.path}`,
+              changefreq: `weekly`,
+              priority: 0.7,
+            }
+          }),
+      },
+    },
     `gatsby-plugin-robots-txt`,
   ],
 }
