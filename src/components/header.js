a51ce7b8d5c5588bbc1e16b9a7e43b3a2a525c78
Cesar Murillo <namiayumis@gmail.com>
2020-07-13 16:14:21 +0900

    install robots text

--- src/components/header.js
+++ src/components/header.js
@@ -17,29 +17,27 @@ const Header = () => (
           <Link to="/">日本語</Link>
         </div>
         <div className="nav-social"></div>
-        <div className="nav-menu">
-          <input
-            type="checkbox"
-            id="menu"
-            name="menu"
-            className="menu__checkbox"
-          />
-          <label htmlFor="menu" className="menu__button"></label>
-          <ul className="menu">
-            <li className="menu__item">
-              <Link to="/">INICIO</Link>
-            </li>
-            <li className="menu__item">
-              <Link to="/embajador">EMBAJADOR</Link>
-            </li>
-            <li className="menu__item">
-              <Link to="/funcionarios">FUNCIONARIOS</Link>
-            </li>
-            <li className="menu__item">
-              <Link to="/amistad">AMISTAD PERÚ-JAPÓN</Link>
-            </li>
-          </ul>
-        </div>
+        <input
+          type="checkbox"
+          id="menu"
+          name="menu"
+          className="menu__checkbox"
+        />
+        <label htmlFor="menu" className="menu__button"></label>
+        <ul className="menu">
+          <li className="menu__item">
+            <Link to="/">INICIO</Link>
+          </li>
+          <li className="menu__item">
+            <Link to="/embajador">EMBAJADOR</Link>
+          </li>
+          <li className="menu__item">
+            <Link to="/funcionarios">FUNCIONARIOS</Link>
+          </li>
+          <li className="menu__item">
+            <Link to="/amistad">AMISTAD PERÚ-JAPÓN</Link>
+          </li>
+        </ul>
       </nav>
     </div>
   </header>
