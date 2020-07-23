963728a6fca9c038095a6bd56f1afc42f1e98f74
Cesar Murillo <namiayumis@gmail.com>
2020-07-23 14:08:38 +0900

    fix styled heading -> header.js

--- src/components/header.js
+++ src/components/header.js
@@ -7,8 +7,17 @@ import "./header.scss"
 const Heading = styled.h1`
   padding: 0 0px;
   font-size: 0.8rem;
-`
+  width: 142px;
+  margin-left: 0.7rem;
 
+  @media screen and (min-width: 767px) {
+    margin-left: -10.2rem;
+  }
+
+  @media screen and (min-width: 1024px) {
+    margin-left: 1.2rem;
+  }
+`
 
 const Header = () => (
   <header>
@@ -16,9 +25,7 @@ const Header = () => (
       <nav>
         <Escudo></Escudo>
         <Link to="/">
-          <Heading>
-            EMBAJADA DEL PERÚ EN JAPÓN
-          </Heading>
+          <Heading>EMBAJADA DEL PERÚ EN JAPÓN</Heading>
         </Link>
         <div className="nav-language">
           <Link to="/jp">日本語</Link>
