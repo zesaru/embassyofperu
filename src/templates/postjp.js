a6c74696fca993afbc7d247344983539484f453d
Cesar Murillo <namiayumis@gmail.com>
2020-07-24 10:38:43 +0900

    create template postjp

---
+++ src/templates/postjp.js
@@ -0,0 +1,44 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import Image from "gatsby-image"
+import Asidecomunicados from "../components/asidecomunicados"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import "./post.scss"
+
+export const query = graphql`
+  query($slug: String!) {
+    contentfulPosts(slug: { eq: $slug }) {
+      name
+      content {
+        json
+      }
+      images {
+        file {
+          url
+        }
+        fluid(maxWidth: 900) {
+          ...GatsbyContentfulFluid_withWebp
+        }
+      }
+    }
+  }
+`
+const post = props => {
+  return (
+    <Layout>
+      <div className="wrapper post">
+        <div className="content_post">
+          <Image fluid={props.data.contentfulPosts.images[0].fluid} alt="" />
+          <h2 className="post_title">{props.data.contentfulPosts.name}</h2>
+          {documentToReactComponents(props.data.contentfulPosts.content.json)}
+        </div>
+        <aside className="wrapper">
+          <Asidecomunicados></Asidecomunicados>
+        </aside>
+      </div>
+    </Layout>
+  )
+}
+
+export default post
