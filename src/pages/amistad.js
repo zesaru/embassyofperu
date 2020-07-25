48ab318b570b60be3389bb041ba83684c1e91420
Cesar Murillo <namiayumis@gmail.com>
2020-07-25 15:21:48 +0900

    fix bad path actividades and amistaf

--- src/pages/amistad.js
+++ src/pages/amistad.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import Banner from "../components/bannerjp"
+import Banner from "../components/banner"
 
 const amistad = () => {
   return (
