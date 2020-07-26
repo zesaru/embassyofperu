7a0cd358be0fd89785d7821ada8cb1f7480aacfe
Cesar Murillo <namiayumis@gmail.com>
2020-07-26 21:19:23 +0900

    add seo component in posts templates

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import { INLINES } from "@contentful/rich-text-types"
+import SEO from "../components/seo"
 import Layout from "../components/layout"
 import Image from "gatsby-image"
 
@@ -48,9 +49,13 @@ export const query = graphql`
 const post = props => {
   return (
     <Layout>
+      <SEO title={props.data.contentfulPosts.name}></SEO>
       <div className="wrapper post">
         <div className="content_post">
-          <Image fluid={props.data.contentfulPosts.images[0].fluid} alt="" />
+          <Image
+            fluid={props.data.contentfulPosts.images[0].fluid}
+            alt={props.data.contentfulPosts.name}
+          />
           <h2 className="post_title">{props.data.contentfulPosts.name}</h2>
           {documentToReactComponents(
             props.data.contentfulPosts.content.json,
--- src/templates/postjp.js
+++ src/templates/postjp.js
@@ -1,7 +1,8 @@
 import React from "react"
 import { graphql } from "gatsby"
-import Image from "gatsby-image"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import Image from "gatsby-image"
+import SEO from "../components/seo"
 import Layout from "../pages/jp/components/layout-jp"
 import Asidejp from "../components/asidejp"
 import "./post.scss"
@@ -27,6 +28,7 @@ export const query = graphql`
 const post = props => {
   return (
     <Layout>
+      <SEO title={props.data.contentfulPostjp.name}></SEO>
       <div className="wrapper post">
         <div className="content_post">
           <Image fluid={props.data.contentfulPostjp.image[0].fluid} alt="" />
