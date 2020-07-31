7a7ac88eee082555f92bb9905073e65df153fa68
Cesar Murillo <namiayumis@gmail.com>
2020-07-31 17:58:05 +0900

    404 page first version

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,34 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Img from "gatsby-image"
+import { useStaticQuery, graphql } from "gatsby"
 
-const NotFoundPage = () => (
+const NotFoundPage = () => {
+  const data = useStaticQuery(graphql`
+  query {
+    placeholderImage: file(relativePath: { eq: "llama.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 500) {
+          ...GatsbyImageSharpFluid_noBase64
+        }
+      }
+    }
+  }
+`)
+return(
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  <SEO title="404: Not found" />
+  <div className="wrapper">
+    <h1>PAGINA NO ENCONTRADA</h1>
+    <p>La pagina que esta buscando no se encuentra por favor regrese al inicio</p>
+    <Img style="width:500px;"
+          alt="imagen de llama"
+          fluid={data.placeholderImage.childImageSharp.fluid}
+        ></Img>
+  </div>
+</Layout>
 )
+}
 
 export default NotFoundPage
