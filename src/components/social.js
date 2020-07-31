075fabc62ca4373af232f03f1064336191c4d6a0
Cesar Murillo <namiayumis@gmail.com>
2020-07-31 11:21:56 +0900

    center icons DivSocial

--- src/components/social.js
+++ src/components/social.js
@@ -3,11 +3,11 @@ import Img from "gatsby-image"
 import { useStaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
 
-const SocialContainer = styled.div(`
+const DivSocial = styled.div`
   padding: 10px 0px;
   display: flex;
   justify-content: space-around;
-`)
+`
 
 const Social = () => {
   const data = useStaticQuery(graphql`
@@ -29,15 +29,15 @@ const Social = () => {
     }
   `)
   return (
-    <SocialContainer className="wrapper">
+    <DivSocial className="wrapper">
       {data.allContentfulGeneralSetting.edges.map(edge => {
         return (
           <a target="_blank" rel="noreferrer" href={edge.node.url}>
-            <Img fixed={edge.node.image.fixed} alt="facebook logo" />
+            <Img fixed={edge.node.image.fixed} alt={edge.node.name} />
           </a>
         )
       })}
-    </SocialContainer>
+    </DivSocial>
   )
 }
 
