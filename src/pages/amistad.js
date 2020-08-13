de4d57807ec5c02f1fe93159a74bac43e9b723f7
Cesar Murillo <namiayumis@gmail.com>
2020-08-13 12:52:49 +0900

    add contentful page to amistad

--- src/pages/amistad.js
+++ src/pages/amistad.js
@@ -2,80 +2,39 @@ import React from "react"
 import Layout from "../components/layout"
 import Banner from "../components/banner"
 import SEO from "../components/seo"
+import { graphql, useStaticQuery } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
-const amistad = () => {
-  const title = "Amistad Perú-Japón"
+
+const Amistad = () => {
+  const data = useStaticQuery(graphql`
+  query  {
+      allContentfulPage {
+        nodes {
+          title
+          description {
+            json
+          }
+        }
+      }
+    }
+    
+  `)
+
+  const title = data.allContentfulPage.nodes[0].title
   return (
     <Layout>
       <SEO title={title}></SEO>
       <div className="wrapper">
         <Banner></Banner>
         <h2>{title}</h2>
-        <p>
-          El Perú fue el primer país latinoamericano y el decimocuarto del mundo
-          que estableció relaciones diplomáticas con el Japón, mediante el
-          Tratado de Paz, Amistad, Comercio y Navegación, el 21 de agosto en
-          1873, sólo cinco años después de la Restauración Meiji.
-        </p>
-        <p>
-          En la actualidad se estima aproximadamente en ciento cincuenta mil los
-          peruanos descendientes de familias japonesas inmigrantes y cada año,
-          desde 1989, se celebra el 3 de abril el “Día de la Amistad
-          Peruano-Japonesa”, para conmemorar el arribo al Perú de la nave
-          “Sakura Maru” que transporto el primer grupo de inmigrantes japoneses.
-          Actualmente habitan en Japón más de sesenta mil peruanos, siendo ésta
-          la comunidad más numerosa de Hispanoamérica. Asimismo, en el Perú se
-          registra la primera inversión japonesa en un yacimiento de plata cerca
-          de Cerro de Pasco, una de las primeras que realiza este país en el
-          exterior y es el Presidente Manuel Prado el primer Jefe de Estado
-          latinoamericano que visita oficialmente el país del Sol Naciente.
-        </p>
-        <p>
-          A lo largo de nuestra historia en común hemos recibido a importantes
-          altas autoridades japonesas, debiendo destacar las visitas al Perú de
-          las hoy, Sus Majestades Imperiales Akihito y Michiko en 1963, y de Su
-          Alteza Imperial, la Princesa Sayako en 1999, con ocasión de cumplir el
-          Centenario de la Inmigración Japonesa al Perú.
-        </p>
-        <p>
-          En el 2009, el Perú recibió la visita de Sus Altezas los Príncipes
-          Hitachi quienes participaron de diversas actividades conmemorativas de
-          los 110 años de la migración japonesa al Perú. Este ámbito social y
-          humano constituye el verdadero bastión sobre el cual se desarrollan y
-          fundamentan las relaciones bilaterales y el cual ha contribuido a
-          generar un óptimo nivel de vinculación entre ambos Estados.
-        </p>
-        <p>
-          En ese sentido, la Visita Oficial realizada a Japón por el señor
-          Presidente de la República entre el 8 y 10 de mayo pasado, ha sido un
-          marco ideal para dar un nuevo impulso efectivo a las excelentes
-          relaciones bilaterales que existen entre el Perú y Japón
-        </p>
-        <p>
-          Además de lo mencionado anteriormente, debe subrayarse que entre las
-          dos naciones existe una diversidad de vínculos muy importantes
-          también, como el apoyo del Japón a los esfuerzos del gobierno para
-          fortalecer la democracia y el desarrollo socio-económico del Perú y,
-          como labor conjunta, hacer prevalecer y consolidar la paz, la
-          seguridad internacional y la prosperidad del mundo.
-        </p>
-        <p>
-          Otro aspecto muy importante de la relación bilateral, especialmente de
-          cara a la actual era de la globalización, es que el Perú y el Japón,
-          unidos por el Océano Pacífico, coinciden en la conveniencia de
-          mantener vínculos muy estrechos de cooperación mutua en el ámbito
-          multilateral y en foros regionales como el APEC y FEALAC.
-        </p>
-        <p>
-          Constituye una aspiración permanente que en forma conjunta podamos
-          unir esfuerzos para contribuir al desarrollo y a la prosperidad de la
-          Cuenca del Pacífico, el océano más grande y rico del planeta, que
-          tenemos la suerte de compartir y que augura tantos beneficios para las
-          futuras generaciones de peruanos y japoneses.
-        </p>
-      </div>
+        {documentToReactComponents(
+            data.allContentfulPage.nodes[0].description.json,
+            
+          )}
+        </div>        
     </Layout>
   )
 }
 
-export default amistad
+export default Amistad
